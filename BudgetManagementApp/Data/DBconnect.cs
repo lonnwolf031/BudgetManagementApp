ff64a22ce049a4deefd0174c9ad12f48df8a2f26
lonnwolf031 <lonn.wolf031@gmail.com>
2021-09-20 19:16:30 +0200

    -

--- BudgetManagementApp/Data/DBconnect.cs
+++ BudgetManagementApp/Data/DBconnect.cs
@@ -5,6 +5,7 @@ using MySql.Data.MySqlClient;
 using Dapper;
 using System.Threading.Tasks;
 using System.IO;
+using System.Transactions;
 
 namespace BudgetManagementApp.Data
 {
@@ -15,131 +16,20 @@ namespace BudgetManagementApp.Data
         //Constructor
         public DBConnect()
         {
-            connectionString = "SERVER=" + Constants.server + ";" + "DATABASE=" + Constants.database + ";" + "UID=" + Constants.uid. + ";" + "PASSWORD=" + Constants.password + ";";
+            var connectionStringBuilder = new MySqlConnectionStringBuilder
+            {
+                Server = Constants.server,
+                UserID = Constants.uid,
+                Password = Constants.password,
+                Database = Constants.database
+               // CertificateFile = @"<Path_To_The_File>\client.pfx",
+                //CertificatePassword = "<Password_For_The_Cert>"
+            };
+            connectionString = connectionStringBuilder.ToString();
         }
 
-        //Insert statement
-        public void Insert()
-        {
-            string query = "INSERT INTO tableinfo (name, age) VALUES('John Smith', '33')";
 
-            //open connection
-            if (this.OpenConnection() == true)
-            {
-                //create command and assign the query and connection from the constructor
-                MySqlCommand cmd = new MySqlCommand(query, connection);
 
-                //Execute command
-                cmd.ExecuteNonQuery();
-
-                //close connection
-                this.CloseConnection();
-            }
-        }
-
-        //Update statement
-        public void Update()
-        {
-            string query = "UPDATE tableinfo SET name='Joe', age='22' WHERE name='John Smith'";
-
-            //Open connection
-            if (this.OpenConnection() == true)
-            {
-                //create mysql command
-                MySqlCommand cmd = new MySqlCommand();
-                //Assign the query using CommandText
-                cmd.CommandText = query;
-                //Assign the connection using Connection
-                cmd.Connection = connection;
-
-                //Execute query
-                cmd.ExecuteNonQuery();
-
-                //close connection
-                this.CloseConnection();
-            }
-        }
-
-        //Delete statement
-        public void Delete()
-        {
-            string query = "DELETE FROM tableinfo WHERE name='John Smith'";
-
-            if (this.OpenConnection() == true)
-            {
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-                cmd.ExecuteNonQuery();
-                this.CloseConnection();
-            }
-        }
-
-        //Select statement
-        public List<string>[] Select()
-        {
-            string query = "SELECT * FROM tableinfo";
-
-            //Create a list to store the result
-            List<string>[] list = new List<string>[3];
-            list[0] = new List<string>();
-            list[1] = new List<string>();
-            list[2] = new List<string>();
-
-            //Open connection
-            if (this.OpenConnection() == true)
-            {
-                //Create Command
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-                //Create a data reader and Execute the command
-                MySqlDataReader dataReader = cmd.ExecuteReader();
-
-                //Read the data and store them in the list
-                while (dataReader.Read())
-                {
-                    list[0].Add(dataReader["id"] + "");
-                    list[1].Add(dataReader["name"] + "");
-                    list[2].Add(dataReader["age"] + "");
-                }
-
-                //close Data Reader
-                dataReader.Close();
-
-                //close Connection
-                this.CloseConnection();
-
-                //return list to be displayed
-                return list;
-            }
-            else
-            {
-                return list;
-            }
-        }
-
-        //Count statement
-        public int Count()
-        {
-            string query = "SELECT Count(*) FROM tableinfo";
-            int Count = -1;
-
-            //Open Connection
-            if (this.OpenConnection() == true)
-            {
-                //Create Mysql Command
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-
-                //ExecuteScalar will return one value
-                Count = int.Parse(cmd.ExecuteScalar() + "");
-
-                //close Connection
-                this.CloseConnection();
-
-                return Count;
-            }
-            else
-            {
-                return Count;
-            }
-        }
 
         //Backup
         public void Backup()
@@ -255,6 +145,19 @@ namespace BudgetManagementApp.Data
             }
         }
 
+        public async Task AddPerson(string name, string surname)
+        {
+            const string sql = "insert into Persons(Name, Surname) values(@Name, @Surname)";
+
+            using (var tran = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+           // using (var connection = await _connectionProvider.OpenAsync()) //Or however you get the connection
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                await conn.ExecuteAsync(sql, new { name, surname });
+                tran.Complete();
+            }
+        }
+
         public async Task<Account> GetByIdAsync(long id)
         {
             using (var sqlConnection = new MySqlConnection(connectionString))
@@ -286,6 +189,37 @@ namespace BudgetManagementApp.Data
                 var account = accounts.SingleOrDefault();
 
                 return account;
+            }
+        }
+
+        public async Task<List<Artist>> GetAllAsync()
+        {
+            using (
+                SqlConnection conn =
+                    new SqlConnection(Conn.String))
+            {
+                await conn.OpenAsync();
+
+                using (var multi = await conn.QueryMultipleAsync(StoredProcs.Artists.GetAll, commandType: CommandType.StoredProcedure))
+                {
+                    var Artists = multi.Read<Artist, AlbumArtist, Artist>((artist, albumArtist) =>
+                    {
+                        artist.albumArtist = albumArtist;
+                        return artist;
+                    }).ToList();
+
+                    var albums = multi.Read<Album, AlbumArtist, Album>(
+                    (album, albumArtist, album) =>
+                    {
+                        album.albumArtist = album;
+                        return albums;
+                    }).ToList();
+
+
+                    conn.Close();
+
+                    return Artists;
+                }
             }
         }
 
