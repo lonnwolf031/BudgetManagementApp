dff24be9bc4859ff52a216f5b0ad2f0482a36e8e
lonnwolf031 <lonn.wolf031@gmail.com>
2021-09-20 11:57:00 +0200

    updating again

--- BudgetManagementApp/Models/BudgetItem.cs
+++ BudgetManagementApp/Models/BudgetItem.cs
@@ -6,5 +6,12 @@ namespace BudgetManagementApp.Models
 {
     internal class BudgetItem
     {
+        public int Id { get; set; }
+
+        public List<string> Somelist { get; set; }
+        public BudgetItem()
+        {
+            Somelist = new List<string>();
+        }
     }
 }
