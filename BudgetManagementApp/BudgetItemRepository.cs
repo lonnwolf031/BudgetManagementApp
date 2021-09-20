0ea86ba903aa66a2e615388e5e0801d17d5d7de7
lonnwolf031 <lonn.wolf031@gmail.com>
2021-09-20 11:38:53 +0200

    updating

---
+++ BudgetManagementApp/BudgetItemRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetManagementApp.ViewModels;
+using BudgetManagementApp.Models;
+
+namespace BudgetManagementApp
+{
+    class BudgetItemRepository
+    {
+        private static List<BudgetItem> budgetItems = new List<BudgetItem>();
+
+        internal void Add(BudgetItem budgetItem)
+        {
+            budgetItems.Add(budgetItem);
+        }
+        
+        internal void Remove(BudgetItem budgetItem)
+        {
+            budgetItems.Remove(budgetItem);
+        }
+
+        internal BudgetItem Search(int id)
+        {
+            //Get the budgetItems index in the collection
+            int index = GetIndex(id);
+            //If present then return the element
+            if (index > -1)
+                return budgetItems[index];
+            return null;
+        }
+
+        private int GetIndex(int id)
+        {
+            int index = -1;
+            //If Collection has Items
+            if (budgetItems.Count > 0)
+            {
+                //Loop through the collection
+                for (int i = 0; i < budgetItems.Count; i++)
+                {
+                    //If match
+                    if (budgetItems[i].Id == id)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            return index;
+        }
+    }
+}
