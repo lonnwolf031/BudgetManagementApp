de7f19adb30a9122f4a69f3f48b5dacb9d136202
lonnwolf031 <lonn.wolf031@gmail.com>
2021-09-20 15:43:24 +0200

    updating

--- BudgetManagementApp/Data/DeelnemerRepository.cs
+++ BudgetManagementApp/Data/DeelnemerRepository.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BudgetManagementApp.ViewModels;
-using BudgetManagementApp.Models;
+using MVVMbezettingApp.ViewModel;
+using MVVMbezettingApp.Model;
 
-namespace BudgetManagementApp
+namespace MVVMbezettingApp
 {
     class BudgetItemRepository
     {
