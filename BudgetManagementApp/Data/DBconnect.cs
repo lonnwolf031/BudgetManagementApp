454ed01ad41e305d2c0efd052c6e6035147074f0
lonnwolf031 <lonn.wolf031@gmail.com>
2021-10-15 10:09:42 +0200

    all

--- BudgetManagementApp/Data/DBconnect.cs
+++ BudgetManagementApp/Data/DBconnect.cs
@@ -165,73 +165,111 @@ namespace BudgetManagementApp.Data
             }
         }
 
-        /*
-        public async Task<Account> GetByIdAsync(long id)
+    private void Backup()
+    {
+      string constring = "server=localhost;user=root;pwd=qwerty;database=test;";
+      string file = "C:\\backup.sql";
+      using (MySqlConnection conn = new MySqlConnection(constring))
+      {
+        using (MySqlCommand cmd = new MySqlCommand())
         {
-            using (var sqlConnection = new MySqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-
-                var accounts = await sqlConnection.QueryAsync<Account>("SELECT * FROM hm_accounts WHERE accountid = @accountid", new
-                {
-                    accountid = id
-                });
-
-                var account = accounts.SingleOrDefault();
-
-                return account;
-            }
+          using (MySqlBackup mb = new MySqlBackup(cmd))
+          {
+            cmd.Connection = conn;
+            conn.Open();
+            mb.ExportToFile(file);
+            conn.Close();
+          }
         }
-
-        public async Task<Account> GetByNameAsync(string address)
-        {
-            using (var sqlConnection = new MySqlConnection(connectionString))
-            {
-                sqlConnection.Open();
-
-                var accounts = await sqlConnection.QueryAsync<Account>("SELECT * FROM hm_accounts WHERE accountaddress = @accountaddress", new
-                {
-                    accountaddress = address
-                });
-
-                var account = accounts.SingleOrDefault();
-
-                return account;
-            }
-        }
-
-        public async Task<List<Artist>> GetAllAsync()
-        {
-            using (
-                SqlConnection conn =
-                    new SqlConnection(Conn.String))
-            {
-                await conn.OpenAsync();
-
-                using (var multi = await conn.QueryMultipleAsync(StoredProcs.Artists.GetAll, commandType: CommandType.StoredProcedure))
-                {
-                    var Artists = multi.Read<Artist, AlbumArtist, Artist>((artist, albumArtist) =>
-                    {
-                        artist.albumArtist = albumArtist;
-                        return artist;
-                    }).ToList();
-
-                    var albums = multi.Read<Album, AlbumArtist, Album>(
-                    (album, albumArtist, album) =>
-                    {
-                        album.albumArtist = album;
-                        return albums;
-                    }).ToList();
-
-
-                    conn.Close();
-
-                    return Artists;
-                }
-            }
-        }
-        */
-
-
+      }
     }
+
+    private void Restore()
+    {
+      string constring = "server=localhost;user=root;pwd=qwerty;database=test;";
+      string file = "C:\\backup.sql";
+      using (MySqlConnection conn = new MySqlConnection(constring))
+      {
+        using (MySqlCommand cmd = new MySqlCommand())
+        {
+          using (MySqlBackup mb = new MySqlBackup(cmd))
+          {
+            cmd.Connection = conn;
+            conn.Open();
+            mb.ImportFromFile(file);
+            conn.Close();
+          }
+        }
+      }
+    }
+
+    /*
+    public async Task<Account> GetByIdAsync(long id)
+    {
+        using (var sqlConnection = new MySqlConnection(connectionString))
+        {
+            sqlConnection.Open();
+
+            var accounts = await sqlConnection.QueryAsync<Account>("SELECT * FROM hm_accounts WHERE accountid = @accountid", new
+            {
+                accountid = id
+            });
+
+            var account = accounts.SingleOrDefault();
+
+            return account;
+        }
+    }
+
+    public async Task<Account> GetByNameAsync(string address)
+    {
+        using (var sqlConnection = new MySqlConnection(connectionString))
+        {
+            sqlConnection.Open();
+
+            var accounts = await sqlConnection.QueryAsync<Account>("SELECT * FROM hm_accounts WHERE accountaddress = @accountaddress", new
+            {
+                accountaddress = address
+            });
+
+            var account = accounts.SingleOrDefault();
+
+            return account;
+        }
+    }
+
+    public async Task<List<Artist>> GetAllAsync()
+    {
+        using (
+            SqlConnection conn =
+                new SqlConnection(Conn.String))
+        {
+            await conn.OpenAsync();
+
+            using (var multi = await conn.QueryMultipleAsync(StoredProcs.Artists.GetAll, commandType: CommandType.StoredProcedure))
+            {
+                var Artists = multi.Read<Artist, AlbumArtist, Artist>((artist, albumArtist) =>
+                {
+                    artist.albumArtist = albumArtist;
+                    return artist;
+                }).ToList();
+
+                var albums = multi.Read<Album, AlbumArtist, Album>(
+                (album, albumArtist, album) =>
+                {
+                    album.albumArtist = album;
+                    return albums;
+                }).ToList();
+
+
+                conn.Close();
+
+                return Artists;
+            }
+        }
+    }
+    */
+
+
+  }
 }
