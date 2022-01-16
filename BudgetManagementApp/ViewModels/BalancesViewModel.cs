933212ea81e032459a3c2d24819facc95d07dfd6
lonnwolf031 <lonn.wolf031@gmail.com>
2022-01-16 11:09:05 +0100

    ~

--- BudgetManagementApp/ViewModels/BalancesViewModel.cs
+++ BudgetManagementApp/ViewModels/BalancesViewModel.cs
@@ -1,33 +1,58 @@
-﻿using BudgetManagementApp.Models;
+﻿using BudgetManagementApp.Data;
+using BudgetManagementApp.Models;
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
 namespace BudgetManagementApp.ViewModels
 {
-  public class BalancesViewModel : INotifyPropertyChanged
+
+  public class BalancesViewModel : IRepository<Balance>, INotifyPropertyChanged
   {
-    private BalanceRepository balanceRepo = new BalanceRepository();
+
     private ObservableCollection<Balance> balances;
     public event PropertyChangedEventHandler PropertyChanged;
 
-    public  ObservableCollection<Balance> Balances
+
+    public BalancesViewModel()
     {
-      get { return balanceRepo.Items; }
+      balances = new ObservableCollection<Balance>();
+    }
+
+    public void DeleteItem(int id)
+    {
+      DBhandler.Instance.RemoveBalance(id);
+    }
+
+    public Balance GetTByID(int itemId)
+    {
+      return DBhandler.Instance.GetBalanceByID(itemId);
+    }
+
+    public void InsertItem(Balance item)
+    {
+      DBhandler.Instance.InsertBalance(item);
+    }
+
+
+    public void UpdateItem(Balance item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public ObservableCollection<Balance> Items
+    {
+      get => balances = DBhandler.Instance.GetAllBalances();
       set
       {
         if (balances != value) {
           balances = value;
           if (PropertyChanged != null) {
-            PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+            PropertyChanged(this, new PropertyChangedEventArgs("Balances"));
           }
         }
         balances = value;
       }
-    }
-
-    public  BalancesViewModel()
-    {
-
     }
   }
 }
