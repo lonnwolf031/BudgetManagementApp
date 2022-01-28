e990379276da39336658c83b043cb00fd1e00b64
lonnwolf031 <lonn.wolf031@gmail.com>
2022-01-28 10:10:57 +0100

    ~

--- BudgetManagementApp/Views/BalancesPage.xaml.cs
+++ BudgetManagementApp/Views/BalancesPage.xaml.cs
@@ -15,12 +15,79 @@ namespace BudgetManagementApp.Views
       InitializeComponent();
       balancesViewModel = new BalancesViewModel();
       this.BindingContext = balancesViewModel;
-      listView.ItemSelected += listSelection;
+      // listView.ItemSelected += listSelection;
 
-      listView.ItemTemplate = new DataTemplate(typeof(EntryCell));
-      listView.ItemTemplate.SetBinding(EntryCell.LabelProperty, "comment");
-      listView.ItemTemplate.SetBinding(EntryCell.TextProperty, "name");
-      Content = listView;
+      LoadUI();
+
+      //// listView.ItemTemplate = new DataTemplate(typeof(Label));
+
+      //var dataTemplate = new DataTemplate();
+      //var nameLabel = new Label();
+      //nameLabel.SetBinding(Label.TextProperty, "Name");
+      //dataTemplate.Bindings.Add
+      ////titleLabel.SetBinding(Label.TextProperty, "Title");
+
+      //listView.ItemTemplate = dataTemplate;
+      //listView.ItemTemplate.SetBinding(EntryCell.LabelProperty, "comment");
+      //listView.ItemTemplate.SetBinding(EntryCell.TextProperty, "name");
+      // Content = listView;
+    }
+
+    private void LoadUI()
+    {
+      var dataTemplate = new DataTemplate(() =>
+      {
+        var nameLabel = new Label();
+        nameLabel.SetBinding(Label.TextProperty, "Name");
+
+        var latestUpdateLabel = new Label();
+        latestUpdateLabel.SetBinding(Label.TextProperty, "LatestUpdate");
+
+        return new StackLayout
+        {
+          Orientation = StackOrientation.Horizontal,
+          Padding = 1,
+          HorizontalOptions = LayoutOptions.FillAndExpand,
+          Children = {
+            new Frame {
+              Content = new AbsoluteLayout {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = {
+                  new StackLayout {
+                    Margin = new Thickness(20),
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    Children = {
+                      nameLabel,
+                      latestUpdateLabel
+                    }
+                  }
+                }
+              }
+            }
+        }
+        };
+      });
+
+      var listView = new ListView();
+      listView.SetBinding(ListView.ItemsSourceProperty, "Items");
+      listView.ItemTemplate = dataTemplate;
+      listView.BackgroundColor = Color.LightGoldenrodYellow;
+      //listView.FlowColumnCount = 1;
+      listView.HasUnevenRows = true;
+
+      var button = new Button { Text = "Add" };
+      //button.Clicked += Button_Clicked
+      //;
+      Content = new StackLayout
+      {
+        Children = {
+                    button,
+                    listView
+                }
+      };
+
     }
 
     protected async override void OnAppearing()
